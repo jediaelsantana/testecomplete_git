9679ffabf0ea3e6ab9d6052c78329e333d31db92
jediael.luiz.santana@accenture.com <jediael.luiz.santana@accenture.com>
2019-05-10 14:56:34 -0300

    Test Daniel New Script Update Functions

--- projeto_02_java_script/criacaoOrdeJavaScript/Script/Functions.js
+++ projeto_02_java_script/criacaoOrdeJavaScript/Script/Functions.js
@@ -5,7 +5,7 @@
  Project.Variables.DataAtual 
   if (data_formatada)
   {
-    = data_formatada;
+    //= data_formatada;
     Log.Message("Data Atual gerada com Sucesso!!!!!, Valor gerado foi: '"+data_formatada+"'");
   }else {
     Log.Error("ERRO! Não foi gerada a data atual no método dataAtual(), o valor retornado foi: '"+data_formatada+"'");
